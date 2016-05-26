cd0e39b1a32666479a881297868668a2a712ff9c
jvs <john.von.seneca@gmail.com>
2016-05-26 18:23:50 +0530

    cache: persist, read from disk

--- src/utils/cache.py
+++ src/utils/cache.py
@@ -1,12 +1,14 @@
 import os
+import hashlib
+import csv
+
 
 class Cache(object):
 	def __init__(self, fun):
 		self.fun = fun
-		self.cache = {}
 		self.location = os.path.abspath('.') + '/data/cache/'
+		self.file_keys = self.location + 'keys.csv'
 		self._read_cache()
-		print(self.location)
 
 	def __get__(self, obj, objtype):
 		"""Support instance methods."""
@@ -14,17 +16,27 @@ class Cache(object):
 		return functools.partial(self.__call__, obj)
 
 	def _read_cache(self):
-		pass
+		self.cache = {}
+		for key, val_md5 in csv.reader(open(self.file_keys)):
+			self.cache[key] = open(self.location + val_md5, 'r').read()
 
 	def __call__(self, *args, **kwargs):
-		key	 = str(args) + str(kwargs)
-		print('key: ', key)
+		# the first arg is the object id, shouldn't include that in the key
+		key	 = str(args[1:]) + str(kwargs)
 		try:
 			return self.cache[key]
 		except KeyError:
+			print('key ', key, ' not found ... ')
 			self.cache[key] = rval = self.fun(*args, **kwargs)
-			print(type(rval))
+			self._append_key(key, rval)
 			return rval
 		except TypeError: # incase key isn't a valid key - don't cache
 			return self.fun(*args, **kwargs)
 	
+
+	def _append_key(self, key, val):
+		csv_writer = csv.writer(open(self.file_keys, "a"))
+		val_md5 = hashlib.md5(val.encode('utf-8')).hexdigest()
+		csv_writer.writerow([key, val_md5])
+		with open(self.location + val_md5, 'w+') as file_val:
+			file_val.write(val)
