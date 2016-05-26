d1df7723dc69d21b267a129681f2b6382b9bda37
jvs <john.von.seneca@gmail.com>
2016-05-26 18:50:28 +0530

    pami/all_issues

--- src/adapters/pami_adapter.py
+++ src/adapters/pami_adapter.py
@@ -10,16 +10,42 @@ class PamiAdapter(Adapter):
 		Adapter.__init__(self)
 
 	def parse(self, url):
-		self.soup = Adapter.parse(self, url)
+		return Adapter.parse(self, url)
 
-	def _div_years(self):
-		return self.soup.find_all('div', 'volume')
-		
+	def _div_years(self, soup):
+		return soup.find_all('div', 'volume')
+
+	def _hashie(self, element, name_key, name_val):
+		return {element[name_key]: element[name_val]}
+
+	def _slice(self, element, *args):
+		dict1 = {}
+		for arg in args:
+			dict1[arg] = element[arg]
+		return dict1
+	
 	def all_years(self):
-		self.parse(PamiAdapter.URL)
-		links_relative = [div.find_all('a')[0]['href'] for div in self._div_years()]
-		links_absolute = [self.URL_ROOT+link for link in links_relative]
-		return links_absolute
+		soup = self.parse(PamiAdapter.URL)
+		links = [self._slice(div.find_all('a')[0],'title','href') for div in self._div_years(soup)]
+		for link in links:
+			link['href'] = PamiAdapter.URL_ROOT + link['href']
+		return links
+
+	def _get_issues(self, url):
+		soup = self.parse(url)
+		div_issues = soup.find_all('div', 'issuePeriod')
+		links = [self._slice(div.find_all('a')[0], 'title', 'href') for div in div_issues]
+		for link in links:
+			link['href'] = PamiAdapter.URL_ROOT + link['href']
+		return links
+
+	def all_issues(self):
+		info_volumes = self.all_years()
+		for info_volume in info_volumes:
+			issues_current = self._get_issues(info_volume['href'])
+			info_volume['issues'] = issues_current
+		return info_volumes
+			
 
 	
 			
--- start-yo.py
+++ start-yo.py
@@ -5,8 +5,12 @@ from src.adapters.pami_adapter import *
 
 pami = PamiAdapter()
 #print(pami.soup.prettify())
-print(pami.all_years())
+# print(pami.all_years())
 
+
+url = "https://www.computer.org/csdl/trans/tp/2016/index.html"
+# print(pami._get_issues(url))
+print(pami.all_issues())
 
 
 
