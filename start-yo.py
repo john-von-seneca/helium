4e3e064f67773f865c749979e807ee2e45b02501
jvs <john.von.seneca@gmail.com>
2016-05-26 19:33:21 +0530

    cool

--- start-yo.py
+++ start-yo.py
@@ -2,22 +2,20 @@
 
 
 from src.adapters.pami_adapter import *
+from src.third_party.scholar import *
 
 pami = PamiAdapter()
-#print(pami.soup.prettify())
-# print(pami.all_years())
 
+# url = 'https://www.computer.org/csdl/trans/tp/2016/06/index.html'
+# print(pami._get_toc_of_issue(url))
 
-#url = "https://www.computer.org/csdl/trans/tp/2016/index.html"
-# print(pami._get_issues(url))
-#print(pami.all_issues())
-
-
-url = 'https://www.computer.org/csdl/trans/tp/2016/06/index.html'
-print(pami._get_toc_of_issue(url))
-
-
-
+querier = ScholarQuerier()
+query = SearchScholarQuery()
+query.set_phrase(options.phrase)
+query.set_num_page_results(3)
+querier.send_query(query)
+#txt(querier, None)
+[print(encode(art.as_txt()) + '\n') for art in querier.articles]
 
 
 
